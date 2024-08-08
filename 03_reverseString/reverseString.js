725f3c854ce28279403d616d1822fa23d1903d16
scheuringtamas <sch.tamas5@gmail.com>
2024-08-08 14:11:55 +0200

    Solution for 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (stringInput) {
+  let result = "";
+  for (let i = stringInput.length - 1; i >= 0; i--) {
+    result += stringInput[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
