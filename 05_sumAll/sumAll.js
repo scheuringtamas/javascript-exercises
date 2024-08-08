2efd516ca980dde8cfc46e5f08e7f0948f441038
scheuringtamas <sch.tamas5@gmail.com>
2024-08-08 20:01:15 +0200

    Solution for 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+  let total = 0;
+  if (
+    isNaN(arg1) ||
+    isNaN(arg2) ||
+    arg1 < 0 ||
+    arg2 < 0 ||
+    !Number.isInteger(arg1) ||
+    !Number.isInteger(arg2)
+  ) {
+    return "ERROR";
+  } else {
+    let smaller = 0;
+    let bigger = 0;
+    let sum = 0;
 
+    if (arg1 < arg2) {
+      smaller = arg1;
+      bigger = arg2;
+    } else {
+      smaller = arg2;
+      bigger = arg1;
+    }
+
+    for (let i = smaller; i <= bigger; i++) {
+      sum += i;
+    }
+
+    return sum;
+  }
 };
 
 // Do not edit below this line
