998b1481644ddf76d8fa8af84d12e7daf87c6c59
scheuringtamas <sch.tamas5@gmail.com>
2024-08-11 16:26:04 +0200

    Solution for 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+  const punctuation = arg
+    .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, "")
+    .toLowerCase();
+  //const reversed = punctuation.split("").reduce((acc, char) => char + acc, "");
+  const reversed = punctuation.split("").reverse().join("");
+  return reversed === punctuation;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
