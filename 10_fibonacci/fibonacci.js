2e1e0f357c484f943cfe3151a734fa3713db4172
scheuringtamas <sch.tamas5@gmail.com>
2024-08-11 16:49:05 +0200

    Solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (arg) {
+  const parseArg = parseInt(arg);
+  if (parseArg === 0 || parseArg === 1) {
+    return parseArg;
+  } else if (parseArg < 0) {
+    return "OOPS";
+  } else {
+    return fibonacci(parseArg - 1) + fibonacci(parseArg - 2);
+  }
 };
 
 // Do not edit below this line
